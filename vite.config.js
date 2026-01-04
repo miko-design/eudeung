4f8ce0d13d2f492cf52532041ca65fa741503262
meekclay <meekclay@gmail.com>
2026-01-04 15:28:52 +0900

    Fix: vite config base path 추가

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 배포를 위해 리포지토리 이름을 경로로 설정합니다.
+  base: '/eudeung/',
 })
